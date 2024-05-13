018c37a355cfd161b72710b71df9b6d2e3d10856
Akash Kharita <akharita1999@gmail.com>
2024-05-13 10:50:01 -0700

    Added exception handling in the feature extraction

--- feature_extraction_scripts/physical_feature_extraction_scripts/new_physical_feature_extraction_noise_50_100.py
+++ feature_extraction_scripts/physical_feature_extraction_scripts/new_physical_feature_extraction_noise_50_100.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 import calendar
 from tsfel import time_series_features_extractor
 
-
 import seis_feature
+
 
 
 
@@ -43,60 +43,27 @@ pd.set_option('display.max_columns', None)
 import time
 
 
-        
-              
-              
 
               
-exotic_file_name = "/data/whd01/yiyu_data/PNWML/exotic_waveforms.hdf5"
-exotic_csv_file = pd.read_csv("/data/whd01/yiyu_data/PNWML/exotic_metadata.csv")
-f = h5py.File(exotic_file_name, 'r')
+noise_file_name = "/data/whd01/yiyu_data/PNWML/noise_waveforms.hdf5"          
+noise_csv_file = pd.read_csv("/data/whd01/yiyu_data/PNWML/noise_metadata.csv")
+f = h5py.File(noise_file_name, 'r')          
+buckets = [noise_csv_file['trace_name'].values[i].split('$')[0] for i in range(len(noise_csv_file))]
+indices = [int(noise_csv_file['trace_name'].values[i].split('$')[1].split(',')[0]) for i in range(len(noise_csv_file))]
+buck_no = np.array(buckets)
+ind_no = np.array(indices)
 
-buckets = [exotic_csv_file['trace_name'].values[i].split('$')[0] for i in range(len(exotic_csv_file))]
-indices = [int(exotic_csv_file['trace_name'].values[i].split('$')[1].split(',')[0]) for i in range(len(exotic_csv_file))]
+data_no = []
 
-source = exotic_csv_file['source_type'].values
-
-
-buck_su = np.array(buckets)[np.where(source == 'surface event')[0]]
-ind_su = np.array(indices)[np.where(source == 'surface event')[0]]
-
-data_su = []
-for i in tqdm(range(len(buck_su))):
-     data_su.append(f['/data/'+buck_su[i]][ind_su[i], 2, :])
-        
-        
-data_su = np.array(data_su)
-
-
-
-buck_th = np.array(buckets)[np.where(source == 'thunder')[0]]
-ind_th = np.array(indices)[np.where(source == 'thunder')[0]]
-
-data_th = []
-for i in tqdm(range(len(buck_th))):
-     data_th.append(f['/data/'+buck_th[i]][ind_th[i], 2, :])
-        
-        
-data_th = np.array(data_th)
-
-
-buck_sb = np.array(buckets)[np.where(source == 'sonic boom')[0]]
-ind_sb = np.array(indices)[np.where(source == 'sonic boom')[0]]
-
-data_sb = []
-for i in tqdm(range(len(buck_sb))):
-     data_sb.append(f['/data/'+buck_sb[i]][ind_sb[i], 2, :])
-        
-        
-data_sb = np.array(data_sb) 
+for i in tqdm(range(len(buck_no))):
+     data_no.append(f['/data/'+buck_no[i]][ind_no[i], 2, :])
               
-  
+data_no = np.array(data_no) 
+        
+   
               
               
-       
-            
-        
+              
 def apply_cosine_taper(arrays, taper_percent=10):
     tapered_arrays = []
     
@@ -116,11 +83,7 @@ def apply_cosine_taper(arrays, taper_percent=10):
         
         tapered_arrays.append(tapered_array)
     
-    return np.array(tapered_arrays) 
-              
-              
-              
-              
+    return np.array(tapered_arrays)              
               
               
               
@@ -170,74 +133,36 @@ def butterworth_filter(arrays, lowcut, highcut, fs, num_corners, filter_type='ba
 
               
               
-tapered_su = apply_cosine_taper(data_su, taper_percent = 10) 
-tapered_th = apply_cosine_taper(data_th, taper_percent = 10)  
-tapered_sb = apply_cosine_taper(data_sb, taper_percent = 10)               
-              
+tapered_no = apply_cosine_taper(data_no, taper_percent = 10)
+filtered_no = np.array(butterworth_filter(tapered_no, 1, 10, 100, 4, 'bandpass'))       
 
-filtered_su = np.array(butterworth_filter(tapered_su, 1, 10, 100, 4, 'bandpass'))
-filtered_th = np.array(butterworth_filter(tapered_th, 1, 10, 100, 4, 'bandpass'))
-filtered_sb = np.array(butterworth_filter(tapered_sb, 1, 10, 100, 4, 'bandpass'))
-
-              
-              
-              
-
-
-su_Z = filtered_su
-th_Z = filtered_th
-sb_Z = filtered_sb
-
+no_Z = filtered_no
 
               
 # Normalizing the data.              
+no_Z = no_Z/np.max(abs(no_Z), axis = 1)[:, np.newaxis]
 
-su_Z = su_Z/np.max(abs(su_Z), axis = 1)[:, np.newaxis]
-th_Z = th_Z/np.max(abs(th_Z), axis = 1)[:, np.newaxis]
-sb_Z = sb_Z/np.max(abs(sb_Z), axis = 1)[:, np.newaxis]
 
-              
+no_df_Z = pd.DataFrame([])
+for i in tqdm(range(len(no_Z))): 
+    try:
+        df = seis_feature.FeatureCalculator(no_Z[i]).compute_features()
+        df['serial_no'] = i
+        no_df_Z = pd.concat([no_df_Z, df])
+    except:
+        pass
     
-    
-
-
-
-su_df_Z = pd.DataFrame([])
-for i in tqdm(range(len(su_Z))):
-    df = seis_feature.FeatureCalculator(su_Z[i]).compute_features()
-    df['serial_no'] = i
-    su_df_Z = pd.concat([su_df_Z, df])
-    
-
-th_df_Z = pd.DataFrame([])
-for i in tqdm(range(len(th_Z))):
-    df = seis_feature.FeatureCalculator(th_Z[i]).compute_features()
-    df['serial_no'] = i
-    th_df_Z = pd.concat([th_df_Z, df])
-    
-
-sb_df_Z = pd.DataFrame([])
-for i in tqdm(range(len(sb_Z))):
-    df = seis_feature.FeatureCalculator(sb_Z[i]).compute_features()
-    df['serial_no'] = i
-    sb_df_Z = pd.concat([sb_df_Z, df])
     
 
 
               
-X = pd.concat([ su_df_Z, th_df_Z, sb_df_Z])
-y =  ['surface']*len(su_df_Z)+['thunder']*len(th_df_Z)+['sonic']*len(sb_df_Z)
+X = no_df_Z
+y = ['noise']*len(no_df_Z)
 X['source'] = y
 
 
-
-
-X.to_csv('/home/ak287/PNW_Seismic_Event_Classification/extracted_features/new_physical_features_exotic_z_50_100.csv')
-
-
-
-
-
+ #X.to_csv('/home/ak287/Data_Mining_in_the_PNW/Extracted_Features/Physical_Features/physical_features_noise_z_new.csv')
+X.to_csv('/home/ak287/PNW_Seismic_Event_Classification/extracted_features/new_physical_features_noise_z_50_100.csv')
 
               
               
